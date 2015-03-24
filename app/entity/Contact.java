11558febe88b1a8f47a99b88871858ebfaf9e19a
Valentine_S <staveckij.v@gmail.com>
2015-03-24 16:23:32 +0300

    Add entities: Company, Phone, PhoneType, Contact

--- app/entity/Contact.java
+++ app/entity/Contact.java
@@ -1,5 +1,132 @@
 package entity;
 
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "contact")
 public class Contact {
 
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String middle_name;
+    private Date birthDay;
+    private String email;
+    private Company companyByCompanyId;
+    private String town;
+    private String street;
+    private int house;
+    private int flat;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, insertable = true, updatable = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "first_name", nullable = false, insertable = true, updatable = true)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "last_name", nullable = false, insertable = true, updatable = true)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Basic
+    @Column(name = "midle_name", nullable = true, insertable = true, updatable = true)
+    public String getMiddle_name() {
+        return middle_name;
+    }
+
+    public void setMiddle_name(String middle_name) {
+        this.middle_name = middle_name;
+    }
+
+    @Basic
+    @Column(name = "birth_date", nullable = true, insertable = true, updatable = true)
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    @Basic
+    @Column(name = "email", nullable = true, insertable = true, updatable = true)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @OneToOne
+    @JoinColumn(name = "company_id", referencedColumnName = "id", nullable = false)
+    public Company getCompanyByCompanyId() {
+        return companyByCompanyId;
+    }
+
+    public void setCompanyByCompanyId(Company companyId) {
+        this.companyByCompanyId = companyId;
+    }
+
+    @Basic
+    @Column(name = "town", nullable = true, insertable = true, updatable = true)
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    @Basic
+    @Column(name = "street", nullable = true, insertable = true, updatable = true)
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    @Basic
+    @Column(name = "house", nullable = true, insertable = true, updatable = true)
+    public int getHouse() {
+        return house;
+    }
+
+    public void setHouse(int house) {
+        this.house = house;
+    }
+
+    @Basic
+    @Column(name = "flat", nullable = true, insertable = true, updatable = true)
+    public int getFlat() {
+        return flat;
+    }
+
+    public void setFlat(int flat) {
+        this.flat = flat;
+    }
 }
