5f638b6119374444ca78b710f270514091d458b0
anna-kubarko <anna.kubarko@mail.ru>
2015-03-27 11:43:07 +0300

    Squashed commit of the following:
    commit b7db83c5e2b125c081e54397eb1baebc3fb361ea
    Author: Valentine_S <staveckij.v@gmail.com>
    Date:   Thu Mar 26 12:20:49 2015 +0300
    
         Update Entities, add ContactDAO
    
    commit 793cce8b080188a7faa52e9285721eb94bada79a
    Merge: d8de8b0 7a9d1b5
    Author: Valentine_S <staveckij.v@gmail.com>
    Date:   Thu Mar 26 11:56:35 2015 +0300
    
        Merge remote-tracking branch 'origin/dev' into dev
    
    commit 7a9d1b5428f2bbb2c52d4f2db07d017edbb4c977
    Author: JuliaBorisevska <borisevska.yu@mail.ru>
    Date:   Thu Mar 26 11:40:13 2015 +0300
    
        some view changes
    
    commit d8de8b0f66f8bb99b51a57d94b9b3ba2a25e8497
    Merge: 1b62e5e 64007e6
    Author: Valentine_S <staveckij.v@gmail.com>
    Date:   Thu Mar 26 10:49:44 2015 +0300
    
        Merge remote-tracking branch 'origin/dev' into dev
    
        Conflicts:
        	app/views/login.scala.html
        	app/views/main.scala.html
        	public/stylesheets/main.css
    
    commit 64007e6e80f691146a8cee8550a3cf3ad9edb47c
    Author: JuliaBorisevska <borisevska.yu@mail.ru>
    Date:   Wed Mar 25 13:30:05 2015 +0300
    
        view update
    
    commit 1b62e5e040358fc891ed64df4f92f744929cd452
    Author: Valentine_S <staveckij.v@gmail.com>
    Date:   Tue Mar 24 17:07:21 2015 +0300
    
        Edit login page, main page, main.css
    
    commit 11558febe88b1a8f47a99b88871858ebfaf9e19a
    Author: Valentine_S <staveckij.v@gmail.com>
    Date:   Tue Mar 24 16:23:32 2015 +0300
    
        Add entities: Company, Phone, PhoneType, Contact
    
    commit 469d5423b4bcf2ee15be4ab3d29f228b582e56bd
    Author: Valentine_S <staveckij.v@gmail.com>
    Date:   Tue Mar 24 15:44:05 2015 +0300
    
        Back to OneToOne
    
    commit 9a033a4f2fd6aef3820a400d03ea9e5d8bb59821
    Author: JuliaBorisevska <borisevska.yu@mail.ru>
    Date:   Tue Mar 24 12:40:33 2015 +0300
    
        basic strucrute: DAO, BaseController, dto, view
    
    commit 8846481655e97b2e676b4eaf8e59feabf6eba05b
    Author: Valentine_S <staveckij.v@gmail.com>
    Date:   Mon Mar 23 23:19:33 2015 +0300
    
        Small fix entities
    
    commit cb67599fa3625edf71b30ebc0d7bc26c66345010
    Author: antonkw <antonkw.mail@gmail.com>
    Date:   Mon Mar 23 23:14:10 2015 +0300
    
        Order and OrderHistory entities with Status&User fields
    
    commit 14936de561f1d6e2da246e4bd0490f6441d5a281
    Author: Valentine_S <staveckij.v@gmail.com>
    Date:   Mon Mar 23 22:25:04 2015 +0300
    
        Small fix
    
    commit e6272648e349b3ddd944e9e153437b19285840c9
    Author: Valentine_S <staveckij.v@gmail.com>
    Date:   Mon Mar 23 22:15:24 2015 +0300
    
        Add login.scala.html, add header/footer
    
    commit 4f645ad96fc576a014303df46446e0a3f909f243
    Author: Valentine_S <staveckij.v@gmail.com>
    Date:   Mon Mar 23 00:43:39 2015 +0300
    
        Add entities: Role, Status, User
    
    commit e2f56d3857a95203957011359eba6b0378b25853
    Author: Valentine_S <staveckij.v@gmail.com>
    Date:   Mon Mar 23 00:36:16 2015 +0300
    
        Add entities: Role, Status, User
    
    commit 0fe98bf874057e2549cd17399e06bb4e5b148674
    Author: antonkw <antonkw.mail@gmail.com>
    Date:   Sun Mar 22 16:00:13 2015 +0300
    
        Order and OrderHistory entities
    
    commit 96e31f2e75c675abe307c8d8e631418286392965
    Author: JuliaBorisevska <borisevska.yu@mail.ru>
    Date:   Fri Mar 20 20:57:41 2015 +0300
    
        scripts to database

---
+++ app/dao/OrderDAO.java
@@ -0,0 +1,63 @@
+package dao;
+
+import entity.Order;
+import play.db.jpa.JPA;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+/**
+ * Created by hanna.kubarka on 26.03.2015.
+ */
+public class OrderDAO extends AbstractDAO<Order> {
+
+    public OrderDAO(EntityManager em) {
+        super(em);
+    }
+
+    @Override
+    public void delete(Order entity) {
+
+        EntityManager em = JPA.em();
+        Order order = em.find(Order.class, entity.getId());
+        if(order!=null){
+            em.remove(order);
+        }
+    }
+
+    @Override
+    public void create(Order entity) {
+        JPA.em().persist(entity);
+    }
+
+    @Override
+    public void update(Order entity) {
+        JPA.em().persist(entity);
+
+    }
+
+    public List<Order> getOrderList(Integer pageNumber, Integer pageSize){
+        EntityManager em = JPA.em();
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Order> criteriaQuery = criteriaBuilder.createQuery(Order.class);
+        Root<Order> from = criteriaQuery.from(Order.class);
+
+        CriteriaQuery<Order> select = criteriaQuery.select(from);
+        TypedQuery<Order> q = em.createQuery(select);
+        q.setFirstResult((pageNumber - 1) * pageSize);
+        q.setMaxResults(pageSize);
+
+        return q.getResultList();
+    }
+
+    public Order getOrderById(Integer orderId){
+
+        EntityManager em = JPA.em();
+        return em.find(Order.class, orderId);
+    }
+
+}
