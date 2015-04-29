a315d0c6d5221d0b70c02e98afabb8c56a09d30b
antonkw <antonkw.mail@gmail.com>
2015-04-29 20:17:39 +0300

    new permission handler logic

--- app/handler/RolesHandler.java
+++ app/handler/RolesHandler.java
@@ -16,11 +16,13 @@ import java.util.*;
 public class RolesHandler extends AbstractPrivelegesHandler {
     private static Logger.ALogger logger = Logger.of(RolesHandler.class);
 
-    private Map<String, List<SecurityPermission>> roles = null;
+    private Map<String, List<String>> roles = null;
 
     private Map<String, List<String>> orders = null;
 
     private Map<String, JsonNode> jsonRoles = null;
+
+    private Map<String, List<SecurityPermission>> sectionsPermissions = null;
 
     private StatusHandler statusHandler = null;
 
@@ -36,16 +38,17 @@ public class RolesHandler extends AbstractPrivelegesHandler {
         roles = new HashMap<>();
         orders = new HashMap<>();
         jsonRoles = new HashMap<>();
+        sectionsPermissions = new HashMap<>();
         JsonNode rootNode = mapper.readTree(new File(FILE_CONFIG_NAME));
         List<String> bufferOrderList = null;
         if (!rootNode.has("roles")) {
             throw new ParseException("roles path not exist", 0);
         }
         Iterator<JsonNode> roleNodes = rootNode.path("roles").elements();
-        JsonNode role, arrNode = null;
-        List<SecurityPermission> permissions;
+        JsonNode role, arrNode, section = null;
+        List<String> sections;
         while (roleNodes.hasNext()) {
-            permissions = new ArrayList<>();
+            sections = new ArrayList<>();
             role = roleNodes.next();
             if (!role.has("menu")) {
                 throw new ParseException("menu path not exist", 0);
@@ -57,12 +60,12 @@ public class RolesHandler extends AbstractPrivelegesHandler {
                 }
                 jsonRoles.put(role.path("title").asText(), arrNode);
                 for (final JsonNode objNode : arrNode) {
-                    permissions.add(new SecurityPermission(objNode.asText()));
+                    sections.add(objNode.asText());
                 }
             } else {
                 throw new ParseException("list of permission is not array (check priveleges.json)", 0);
             }
-            roles.put(role.path("title").asText(), permissions);
+            roles.put(role.path("title").asText(), sections);
             if (role.has("order")) {
                 arrNode = role.path("order");
                 if (arrNode.isArray()) {
@@ -76,15 +79,66 @@ public class RolesHandler extends AbstractPrivelegesHandler {
                 }
             }
         }
+        if (!rootNode.has("sections")) {
+            throw new ParseException("roles path not exist", 0);
+        }
+        Iterator<JsonNode> sectionNode = rootNode.path("sections").elements();
+        List<SecurityPermission> permissions = null;
+        while (sectionNode.hasNext()) {
+            permissions = new ArrayList<>();
+            section = sectionNode.next();
+            if (!section.has("id")) {
+                throw new ParseException("id path not exist", 0);
+            }
+            if (section.has("permissions")) {
+                arrNode = section.path("permissions");
+                if (arrNode.isArray()) {
+                    for (JsonNode objNode : arrNode) {
+                        permissions.add(new SecurityPermission(objNode.asText()));
+                    }
+                } else {
+                    throw new ParseException("list of permission is not array (check priveleges.json)", 0);
+                }
+                sectionsPermissions.put(section.path("id").asText(), permissions);
+            }
+
+
+        }
     }
 
     public List<SecurityPermission> getPermissions(SecurityRole role) {
+        logger.info("getting permissions of role {}", role.getName());
         if (roles.containsKey(role.getName())) {
-            return roles.get(role.getName());
+            List<SecurityPermission> permissions = getPermissionsBySections(roles.get(role.getName()));
+            logger.info("count of permissions of role {}: {}", role.getName(), permissions.size());
+            return permissions;
+
         } else {
             logger.warn("role {} is not existing", role.getName());
             return Collections.emptyList();
         }
+    }
+
+    public List<SecurityPermission> getPermissionsBySections(List<String> sections) {
+        logger.info("start getPermissionsBySetting function, count of sections: {}", sections.size());
+        List<SecurityPermission> permissions = new ArrayList<>();
+        for (String section : sections) {
+            if (sectionsPermissions.containsKey(section)) {
+                logger.info("sections-permission map contains section {}", section);
+                for (SecurityPermission permission : sectionsPermissions.get(section)) {
+                    if (!permissions.contains(permission)) {
+                        permissions.add(permission);
+                        logger.info("permission {} add to section {}", permission.getValue(), section);
+                    }
+                    logger.info("permission {} have contained yet at section {}", permission.getValue(), section);
+                }
+            } else {
+                logger.warn("section {} is not existing at section list", section);
+                return Collections.emptyList();
+            }
+        }
+
+        return permissions;
     }
 
     public boolean hasOrders(String title) {
