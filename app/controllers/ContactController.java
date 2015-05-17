82a63a2f4aa6cbcbff96c92d75114e142bc548e1
antonkw <antonkw.mail@gmail.com>
2015-05-17 16:31:06 +0300

    refactor contact controller

--- app/controllers/ContactController.java
+++ app/controllers/ContactController.java
@@ -1,16 +1,12 @@
 package controllers;
 
 import be.objectify.deadbolt.java.actions.Pattern;
-
 import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import dao.ContactDAO;
 import entity.Company;
 import entity.Contact;
 import entity.User;
-
 import org.apache.commons.lang3.StringUtils;
-
 import play.Logger;
 import play.Logger.ALogger;
 import play.db.jpa.JPA;
@@ -30,7 +26,7 @@ import java.util.Map;
 //@Restrict(@Group("!group_name"))
 // @Pattern("!permission_name")
 public class ContactController extends BaseController {
-	private static ALogger logger = Logger.of(ContactController.class);
+    private static ALogger logger = Logger.of(ContactController.class);
 
     @Transactional
     @Pattern("contact_selection")
@@ -38,7 +34,7 @@ public class ContactController extends BaseController {
         logger.info("get contact with id: {}", id);
         ContactDAO contactDAO = new ContactDAO(JPA.em());
         Contact contact = contactDAO.findById(id);
-        if(contact == null) {
+        if (contact == null) {
             return notFound(Json.toJson(new Reply()));
         }
         Reply<Contact> reply = new Reply<>(Status.SUCCESS, contact);
@@ -50,7 +46,7 @@ public class ContactController extends BaseController {
     public static Result listContacts(Integer pageNumber, Integer pageSize) {
         logger.info("get list of contacts, page: {}, size: {}", pageNumber, pageSize);
 
-        if(pageNumber == null || pageSize == null || pageNumber <= 0 || pageNumber <= 0) {
+        if (pageNumber == null || pageSize == null || pageNumber <= 0 || pageNumber <= 0) {
             return badRequest(Json.toJson(new Reply()));
         }
 
@@ -73,7 +69,7 @@ public class ContactController extends BaseController {
                 new Reply<>(Status.SUCCESS, result)
         ));
     }
-    
+
     @Transactional
     @Pattern("contact_search")
     public static Result findContacts() {
@@ -81,11 +77,11 @@ public class ContactController extends BaseController {
         //ContactDAO contactDAO = new ContactDAO(JPA.em());
         final Map<String, String[]> values = request().body().asFormUrlEncoded();
         try {
-        	SearchContactService.searchContacts();
+            SearchContactService.searchContacts();
             //setContactFields(contact, values);
-        	
+
         } catch (Exception e) {
-        	logger.error("Exception in findContacts method: {} ", e);
+            logger.error("Exception in findContacts method: {} ", e);
         }
         return ok(Json.toJson(
                 new Reply<>(Status.SUCCESS, null)
@@ -98,7 +94,7 @@ public class ContactController extends BaseController {
         Contact contact = new Contact();
         User user = Application.recieveUserByToken();
         if (user == null) {
-        	return badRequest(Json.toJson(
+            return badRequest(Json.toJson(
                     new Reply<>(Status.ERROR, MessageManager.getProperty("authentification.error"))));
         }
         Company company = user.getContactByContactId().getCompanyByCompanyId();
@@ -107,11 +103,12 @@ public class ContactController extends BaseController {
         try {
             setContactFields(contact, values);
             contact.setCompanyByCompanyId(company);
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IllegalArgumentException | ClassCastException e) {
+            logger.error("exception during set contact fields", e);
+            return badRequest(Json.toJson(
+                    new Reply<>(Status.ERROR, MessageManager.getProperty("message.error"))));
         }
         contactDAO.create(contact);
-
         return ok(Json.toJson(
                 new Reply<>(Status.SUCCESS, contact)
         ));
@@ -124,96 +121,97 @@ public class ContactController extends BaseController {
         ContactDAO contactDAO = new ContactDAO(JPA.em());
         Contact contact = contactDAO.findById(id);
         final Map<String, String[]> values = request().body().asFormUrlEncoded();
+
         try {
             setContactFields(contact, values);
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        } catch (IllegalArgumentException | ClassCastException e) {
+            logger.error("exception during set contact fields", e);
+            return badRequest(Json.toJson(
+                    new Reply<>(Status.ERROR, MessageManager.getProperty("message.error"))));
         }
         contactDAO.update(contact);
-
         return ok(Json.toJson(
                 new Reply<>(Status.SUCCESS, contact)
         ));
     }
 
-    private static void setContactFields(Contact contact, Map<String, String[]> values) {
+    private static void setContactFields(Contact contact, Map<String, String[]> values) throws IllegalArgumentException, ClassCastException {
+        if (values.containsKey("firstName") || StringUtils.isNotEmpty(values.get("firstName")[0])) {
+            contact.setFirstName(values.get("firstName")[0]);
+        } else {
+            throw new IllegalArgumentException("first name is missing or empty");
+        }
+        if (values.containsKey("lastName") || StringUtils.isNotEmpty(values.get("lastName")[0])) {
+            contact.setLastName(values.get("lastName")[0]);
+        } else {
+            throw new IllegalArgumentException("last name is missing or empty");
+        }
+        if (values.containsKey("middleName") || StringUtils.isNotEmpty(values.get("middleName")[0])) {
+            contact.setMiddleName(values.get("middleName")[0]);
+        } else {
+            throw new IllegalArgumentException("middle name is missing or empty");
+        }
+        if (values.containsKey("birthday")) {
+            contact.setBirthday(Date.valueOf(values.get("birthday")[0]));
+        }
 
-        String firstName = values.get("firstName")[0];
-        String lastName = values.get("lastName")[0];
-        String middleName = values.get("middleName")[0];
-        Date birthDay = Date.valueOf(values.get("birthday")[0]);
-
-        String email = null;
-        String town =  null;
-        String street = null;
+        if (values.containsKey("email") && StringUtils.isNotEmpty(values.get("email")[0])) {
+            contact.setEmail(values.get("email")[0]);
+        }
+        if (values.containsKey("town") && StringUtils.isNotEmpty(values.get("town")[0])) {
+            contact.setTown(values.get("town")[0]);
+        }
+        if (values.containsKey("street") && StringUtils.isNotEmpty(values.get("street")[0])) {
+            contact.setStreet(values.get("street")[0]);
+        }
         Integer house = null;
         Integer flat = null;
-
-        if (StringUtils.isNotEmpty(values.get("email")[0])) {
-            email = values.get("email")[0];
-        }
-        if (StringUtils.isNotEmpty(values.get("town")[0])) {
-            town = values.get("town")[0];
-        }
-
-        if (StringUtils.isNotEmpty(values.get("street")[0])) {
-            street = values.get("street")[0];
-        }
 
         try {
             if (StringUtils.isNotEmpty(values.get("house")[0])) {
                 house = Integer.valueOf(values.get("house")[0]);
+                contact.setHouse(house);
             }
-        } catch (ClassCastException cce) {
-            throw cce;
-        }
-        try {
+
             if (StringUtils.isNotEmpty(values.get("flat")[0])) {
                 flat = Integer.valueOf(values.get("flat")[0]);
+                contact.setFlat(flat);
             }
         } catch (ClassCastException cce) {
             throw cce;
         }
 
-        contact.setFirstName(firstName);
-        contact.setLastName(lastName);
-        contact.setMiddleName(middleName);
-        contact.setBirthday(birthDay);
-        contact.setEmail(email);
-        contact.setTown(town);
-        contact.setStreet(street);
-        contact.setHouse(house);
-        contact.setFlat(flat);
     }
 
     @Transactional
     @Pattern("contact_deleting")
     public static Result deleteContacts(String ids) {
-    	try{
-    		logger.info("Start deleteContacts method with ids: {}", ids);
-    		User user = Application.recieveUserByToken();
+        try {
+            logger.info("Start deleteContacts method with ids: {}", ids);
+            User user = Application.recieveUserByToken();
             if (user == null) {
-    			return badRequest(Json.toJson(
-    		            new Reply<>(Status.ERROR, MessageManager.getProperty("authentification.error"))));
-    		}
+                return badRequest(Json.toJson(
+                        new Reply<>(Status.ERROR, MessageManager.getProperty("authentification.error"))));
+            }
             Company company = user.getContactByContactId().getCompanyByCompanyId();
             java.util.regex.Pattern separator;
-    		separator = java.util.regex.Pattern.compile(",");
-    		String [] idArray = separator.split(ids);
-    		ContactDAO contactDAO = new ContactDAO(JPA.em());
-    		for (String id : idArray){
-    			Contact contact = new Contact();
-    			contact.setCompanyByCompanyId(company);
-    			contact.setId(Long.parseLong(id));
-    			contactDAO.delete(contact);
-    		}
-    		return ok(Json.toJson(
-                new Reply<>(Status.SUCCESS, ids)
-    			));
-    	}catch(Exception e){
-    		logger.error("Error in deleteContacts method: {}", e);
-    		return badRequest(Json.toJson(
-            		new Reply<>(Status.ERROR, MessageManager.getProperty("message.error"))));
-    	}
+            separator = java.util.regex.Pattern.compile(",");
+            String[] idArray = separator.split(ids);
+            ContactDAO contactDAO = new ContactDAO(JPA.em());
+            for (String id : idArray) {
+                Contact contact = new Contact();
+                contact.setCompanyByCompanyId(company);
+                contact.setId(Long.parseLong(id));
+                contactDAO.delete(contact);
+            }
+            return ok(Json.toJson(
+                    new Reply<>(Status.SUCCESS, ids)
+            ));
+        } catch (Exception e) {
+            logger.error("Error in deleteContacts method: {}", e);
+            return badRequest(Json.toJson(
+                    new Reply<>(Status.ERROR, MessageManager.getProperty("message.error"))));
+        }
     }
 }
